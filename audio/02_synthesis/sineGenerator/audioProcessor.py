7dfbd88c2eec0fb44a86311df886ce5c3232adaf
ciskavriezenga <ciskavriezenga@gmail.com>
2017-09-18 21:13:32 +0200

    removed second sine

--- audio/02_synthesis/sineGenerator/audioProcessor.py
+++ audio/02_synthesis/sineGenerator/audioProcessor.py
@@ -4,7 +4,6 @@ class AudioProcessor:
     def __init__(self, audioSettings):
       self.audioSettings = audioSettings
       self.sine = sineGenerator.Sine(audioSettings)
-      self.sine2 = sineGenerator.Sine(audioSettings, 440)
 
     def process(self, inputFrame):
       #move sine a step forward
@@ -12,4 +11,4 @@ class AudioProcessor:
       self.sine2.tick()
       #retrieve the new value
 
-      return self.sine.value + self.sine2.value
+      return self.sine.value
