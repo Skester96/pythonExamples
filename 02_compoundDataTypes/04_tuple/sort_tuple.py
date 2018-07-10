2863b06a981a23786e795eeaa602b16dc989f69c
Ciska Vriezenga <ciskavriezenga@gmail.com>
2018-07-10 15:14:16 +0200

    added small tuple example

---
+++ 02_compoundDataTypes/04_tuple/sort_tuple.py
@@ -0,0 +1,9 @@
+
+
+#TODO - add information, comments, HANDS-ON TIPS
+
+# sequence with midi notes
+sequence = tuple((64, 67, 60))
+print("sequence:", sequence)
+print("sorted sequence:", sorted(sequence))
+print("return type of \"sorted(sequence)\":", type(sorted(sequence)))
